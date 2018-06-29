bd08c186eb2bae724bd32c1e624b5faf5133af65
unknown <markchaitin@gmail.com>
2018-06-28 18:41:28 -0700

    updated gitignore to cull the pycache stuff. also some more input handling refactor

--- handle_keys.py
+++ handle_keys.py
@@ -11,100 +11,113 @@ from level_manager import next_level
 from level_manager import previous_level
 
 
-def handle_keys():
+def input_controller(game_state):
 	tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS | tcod.EVENT_MOUSE, settings.key, settings.mouse)
-	if settings.game_state == 'playing':
-		if settings.key.vk == tcod.KEY_ESCAPE:
-			return 'exit'
-		elif settings.key.vk == tcod.KEY_ENTER and settings.key.lalt:
-			tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
-		# movement keys
-		is_direction = handle_direction_keys()
-		if is_direction is not None:
-			return is_direction
-		key_char = chr(settings.key.c)
-		if key_char == 'g':
-			for obj in settings.objects:
-				if obj.x == settings.player.x and obj.y == settings.player.y and obj.item:
-					obj.item.pick_up()
-					break
-		if key_char == 'i':
-			chosen_item = inventory_menu('Press the key next to the item to use it, or any other key to cancel.\n')
-			if chosen_item is not None:
-				chosen_item.use(settings.player)
-		if key_char == 's':
-			chosen_ability = abilities_menu('Press the key next to the skill to use it, or any other key to cancel.\n')
-			if chosen_ability is not None:
-				for ability in settings.player.combatant.abilities:
-					if ability == chosen_ability:
-						ability.use(settings.player)
-		if key_char == 'd':
-			chosen_item = inventory_menu("Press the letter next to the item to drop it.\n")
-			if chosen_item is not None:
-				chosen_item.drop()
-		if key_char == 'l':
-			settings.game_state = 'looking'
-		if key_char == '.' and settings.key.shift:
-			if settings.stairs_down.x == settings.player.x and settings.stairs_down.y == settings.player.y:
-				next_level()
-		if key_char == ',' and settings.key.shift:
-			if settings.stairs_up.x == settings.player.x and settings.stairs_up.y == settings.player.y:
-				if settings.dungeon_level != 1:
-					previous_level()
-				else:
-					if settings.boner_dome in settings.inventory:
-						msgbox("You escape with the Dome of Boners!")
-						return 'exit'
-					else:
-						msgbox("You cannot leave until you have the Dome of Boners")
-		if key_char == 'c':
-			# show character stats
-			level_up_exp = settings.LEVEL_UP_BASE + settings.player.combatant.level * settings.LEVEL_UP_FACTOR
-			msgbox("Character information\n\nLevel: " + str(settings.player.combatant.level) + "\nExperience: " +
-				str(settings.player.combatant.xp) + "\nExperience to level up:" + str(level_up_exp - settings.player.combatant.xp) +
-				"\n\nMaximum Hp: " + str(settings.player.combatant.max_hp) + "\nAttack: " + str(settings.player.combatant.power) +
-				"\nDefense: " + str(settings.player.combatant.defense), settings.CHARACTER_SCREEN_WIDTH)
+	if settings.key.vk == tcod.KEY_ESCAPE:
+		return 'exit'
+	elif settings.key.vk == tcod.KEY_ENTER and settings.key.lalt:
+		tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
 
-		settings.selection_coordinates = (settings.mouse.cx, settings.mouse.cy)
-	elif settings.game_state == 'looking':
-		is_direction = handle_direction_keys()
-
-		if settings.key.vk == tcod.KEY_ESCAPE:
-			return 'exit'
-		if settings.look_mode == 'mouse':
-			if is_direction is not None:
-				settings.look_mode = 'keyboard'
-			settings.selection_coordinates = (settings.mouse.cx, settings.mouse.cy)
-
-		if settings.look_mode == 'keyboard':
-
-			if settings.old_x is None or settings.old_y is None:
-				settings.old_x = settings.mouse.cx
-				settings.old_y = settings.mouse.cy
-
-			x = settings.old_x
-			y = settings.old_y
-			if is_direction is not None:
-				x = settings.old_x + is_direction[0]
-				y = settings.old_y + is_direction[1]
-				settings.old_x = x
-				settings.old_y = y
-
-			settings.selection_coordinates = (x, y)
-
-			if settings.mouse.dx != 0 or settings.mouse.dy != 0:
-				settings.look_mode = 'mouse'
-				settings.old_x = None
-				settings.old_y = None
-
-		if settings.mouse.lbutton_pressed or settings.key.vk == tcod.KEY_ENTER:
-			information_menu(*settings.selection_coordinates)
-	elif settings.game_state == 'targeting':
-		# figure out how to control this from here
-		pass
-	else:
-		settings.selection_coordinates = (settings.mouse.cx, settings.mouse.cy)
+	if game_state == 'playing':
+		player_action = playing_input()
+		if type(player_action) is tuple:
+			player_move_or_attack(*player_action)
+			return 'time-should-advance'
+	elif game_state == 'looking':
+		looking_input()
+	elif game_state == 'targeting':
+		targeting_input()
 	return 'didnt-take-turn'
+
+
+def playing_input():
+	# movement keys
+	is_direction = handle_direction_keys()
+	if is_direction is not None:
+		return is_direction
+	key_char = chr(settings.key.c)
+	if key_char == 'g':
+		for obj in settings.objects:
+			if obj.x == settings.player.x and obj.y == settings.player.y and obj.item:
+				obj.item.pick_up()
+				break
+	if key_char == 'i':
+		chosen_item = inventory_menu('Press the key next to the item to use it, or any other key to cancel.\n')
+		if chosen_item is not None:
+			chosen_item.use(settings.player)
+	if key_char == 's':
+		chosen_ability = abilities_menu('Press the key next to the skill to use it, or any other key to cancel.\n')
+		if chosen_ability is not None:
+			for ability in settings.player.combatant.abilities:
+				if ability == chosen_ability:
+					ability.use(settings.player)
+	if key_char == 'd':
+		chosen_item = inventory_menu("Press the letter next to the item to drop it.\n")
+		if chosen_item is not None:
+			chosen_item.drop()
+	if key_char == 'l':
+		settings.game_state = 'looking'
+	if key_char == '.' and settings.key.shift:
+		if settings.stairs_down.x == settings.player.x and settings.stairs_down.y == settings.player.y:
+			next_level()
+	if key_char == ',' and settings.key.shift:
+		if settings.stairs_up.x == settings.player.x and settings.stairs_up.y == settings.player.y:
+			if settings.dungeon_level != 1:
+				previous_level()
+			else:
+				if settings.boner_dome in settings.inventory:
+					msgbox("You escape with the Dome of Boners!")
+					return 'exit'
+				else:
+					msgbox("You cannot leave until you have the Dome of Boners")
+	if key_char == 'c':
+		# show character stats
+		level_up_exp = settings.LEVEL_UP_BASE + settings.player.combatant.level * settings.LEVEL_UP_FACTOR
+		msgbox("Character information\n\nLevel: " + str(settings.player.combatant.level) + "\nExperience: " +
+			str(settings.player.combatant.xp) + "\nExperience to level up:" + str(level_up_exp - settings.player.combatant.xp) +
+			"\n\nMaximum Hp: " + str(settings.player.combatant.max_hp) + "\nAttack: " + str(settings.player.combatant.power) +
+			"\nDefense: " + str(settings.player.combatant.defense), settings.CHARACTER_SCREEN_WIDTH)
+
+	settings.selection_coordinates = (settings.mouse.cx, settings.mouse.cy)
+
+
+def looking_input():
+	is_direction = handle_direction_keys()
+
+	if settings.key.vk == tcod.KEY_ESCAPE:
+		return 'exit'
+	if settings.look_mode == 'mouse':
+		if is_direction is not None:
+			settings.look_mode = 'keyboard'
+		settings.selection_coordinates = (settings.mouse.cx, settings.mouse.cy)
+
+	if settings.look_mode == 'keyboard':
+
+		if settings.old_x is None or settings.old_y is None:
+			settings.old_x = settings.mouse.cx
+			settings.old_y = settings.mouse.cy
+
+		x = settings.old_x
+		y = settings.old_y
+		if is_direction is not None:
+			x = settings.old_x + is_direction[0]
+			y = settings.old_y + is_direction[1]
+			settings.old_x = x
+			settings.old_y = y
+
+		settings.selection_coordinates = (x, y)
+
+		if settings.mouse.dx != 0 or settings.mouse.dy != 0:
+			settings.look_mode = 'mouse'
+			settings.old_x = None
+			settings.old_y = None
+
+	if settings.mouse.lbutton_pressed or settings.key.vk == tcod.KEY_ENTER:
+		information_menu(*settings.selection_coordinates)
+
+
+def targeting_input():
+	pass
 
 
 def handle_direction_keys():
--- play_game.py
+++ play_game.py
@@ -2,8 +2,7 @@ import libtcodpy as tcod
 import settings
 
 from check_level_up import check_level_up
-from handle_keys import handle_keys
-from handle_keys import player_move_or_attack
+from handle_keys import input_controller
 from render import render_all
 
 
@@ -17,31 +16,23 @@ def play_game():
 
 	# MAIN LOOP
 	while not tcod.console_is_window_closed():
-		if settings.game_state == 'playing':
-			handle_rendering_tasks()
-			check_level_up()
+		handle_rendering_tasks()
+		check_level_up()
 
-			settings.player_action = handle_keys()
-			# movement action are returned as tuples
-			if type(settings.player_action) is tuple:
-				player_move_or_attack(*settings.player_action)
-			if settings.player_action == 'exit':
-				settings.save_game()
-				break
+		settings.player_action = input_controller(settings.game_state)
+		if settings.player_action == 'exit' and settings.game_state == 'playing':
+			settings.save_game()
+			break
+		elif settings.player_action == 'exit':
+			settings.game_state = 'playing'
 
-			if settings.player_action != 'didnt-take-turn':
-				for obj in settings.objects:
-					if obj.ai:
-						obj.ai.take_turn()
-					if obj.combatant:
-						for ability in obj.combatant.abilities:
-							ability.advance_cooldown_timer()
-		elif settings.game_state == 'looking':
-			handle_rendering_tasks()
-
-			settings.player_action = handle_keys()
-			if settings.player_action == 'exit':
-				settings.game_state = 'playing'
+		if settings.player_action != 'didnt-take-turn':
+			for obj in settings.objects:
+				if obj.ai:
+					obj.ai.take_turn()
+				if obj.combatant:
+					for ability in obj.combatant.abilities:
+						ability.advance_cooldown_timer()
 
 
 def handle_rendering_tasks():
